cd184455f02cdb7794539aa260460e882073bdfe
Ravindra Yadav <yadavra2005@gmail.com>
2025-01-26 17:39:02 +0530

    express-boilerplate

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,16 @@ import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
-const port : number | string = process.env.PORT || 3000;
+const port: number | string = process.env.PORT || 3000;
 
-            app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello from express boilerplate!");
+  res.send("Hello from express boilerplate!");
 });
 
-
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-})
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
